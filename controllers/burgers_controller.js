d0d5cd999e81e919e9e2cda2a0ff00201efb62c2
Kathleen Catlett <kathleencatlett@comcast.net>
2017-03-03 17:43:53 -0500

    need to fix html

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,31 +3,36 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var actor = require('../models/burger.js');
+var burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-	actor.all(function(data){
-		res.render('burgers/index', { burgers: data} )
+	burger.all(function(data){
+		res.render('index', { burgers: data} )
 	})
 	
 });
 
 
 
-router.get("/new", function(req, res) {
-	res.render('burgers/new');
-});
+// router.get("/new", function(req, res) {
+// 	res.render('burgers/new');
+// });
 
 router.post("/create", function(req, res){
 	// res.send(req.body);
 	var cols = ['burger_name', 'devoured'];
 	var vals = [req.body.burgername, false];
 
-	movie.create(cols, vals, function(response){
+	burger.create(cols, vals, function(response){
 		res.redirect('/burgers');
 	});
 })
+
+
+router.put("/update", function(req, res){
+	console.log('put');
+});
 
 
 // Export routes for server.js to use.
